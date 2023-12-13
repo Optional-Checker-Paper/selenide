2985fe79a9c5c1d8036ac09b701300b79e30c0fd
Michael Ernst <mernst@cs.washington.edu>
2023-12-12 22:20:42 -0800

    Remove no-longer-necessary `@SuppressWarnings`

--- src/main/java/com/codeborne/selenide/commands/ancestor/AncestorRuleEngine.java
+++ src/main/java/com/codeborne/selenide/commands/ancestor/AncestorRuleEngine.java
@@ -13,7 +13,6 @@ public class AncestorRuleEngine {
     new AncestorWithAttributeAndValueRule()
   );
 
-  @SuppressWarnings("optional:optional.parameter") // style-optional-parameter
   public AncestorResult process(String selector, int index) {
     return rules
       .stream()
